434ae2cb5aedc3780ae272e7f67f25371e6ef840
Soham Kakra <sk2015@hw.ac.uk>
2021-07-11 18:09:36 +0400

    Inital Commit

---
+++ SimplifyWeb-Frontend/src/app/User.ts
@@ -0,0 +1,12 @@
+export interface User {
+    id: string;
+    firstname: string;
+    lastname: string;
+    emailid: string;
+    role: string;
+    usertype: string;
+    customers: number;
+    updatedOnDate: string;
+    updatedOnTime: string;
+    status: string;
+}
---
+++ SimplifyWeb-Frontend/src/app/mock-users.ts
@@ -0,0 +1,40 @@
+import { User } from "./User"
+
+export const USERS: User[] = [
+    {
+        id: "IU1685",
+        firstname: "Lindsey",
+        lastname: "Stroud",
+        emailid: "lindsey.stroud@gmail.com",
+        role: "Admin",
+        usertype: "Internal",
+        customers: 6,
+        updatedOnDate: "16 Feb 2021",
+        updatedOnTime: "02:30 Pm",
+        status: "Active",
+    },
+    {
+        id: "IU1234",
+        firstname: "Nicci",
+        lastname: "Troiani",
+        emailid: "nicci.troiani@gmail.com",
+        role: "",
+        usertype: "Internal",
+        customers: 0,
+        updatedOnDate: "08 Feb 2021",
+        updatedOnTime: "11:30 Am",
+        status: "Draft",
+    },
+    {
+        id: "IU2456",
+        firstname: "Geroge",
+        lastname: "Fields",
+        emailid: "geroge.fields@gmail.com",
+        role: "Sales Head",
+        usertype: "Internal",
+        customers: 3,
+        updatedOnDate: "28 Feb 2021",
+        updatedOnTime: "04:30 Pm",
+        status: "Pending Approval",
+    }
+]
