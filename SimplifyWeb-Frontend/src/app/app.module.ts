d1147b80947a8d6d0cdd3977d24d3c5bc5e6abc3
Soham Kakra <sk2015@hw.ac.uk>
2021-07-11 18:13:59 +0400

    Header Component Added

--- SimplifyWeb-Frontend/src/app/app.module.ts
+++ SimplifyWeb-Frontend/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { UsersComponent } from './components/users/users.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsersComponent
+    UsersComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
